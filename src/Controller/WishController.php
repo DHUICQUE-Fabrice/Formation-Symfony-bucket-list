2cc313234095090c0826f15d7a3ce255941a5b5f
DHUICQUE-Fabrice <fabrice.dhuicque2021@campus-eni.fr>
2022-01-10 10:29:42 +0100

    TP module 4 - Routage et page About us

---
+++ src/Controller/WishController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/wish", name="wish_")
+ */
+class WishController extends AbstractController
+{
+    /**
+     * @Route("/list", name="list")
+     */
+    public function list(): Response
+    {
+        return $this->render('wish/list.html.twig');
+    }
+
+    /**
+     * @Route("/detail/{id}", name="detail")
+     */
+    public function detail(int $id): Response
+    {
+        return $this->render('wish/detail.html.twig');
+    }
+}
